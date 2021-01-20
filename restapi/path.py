c08a9f73a8570960e111b676d08702c8f71c6cc8
Charles Tolley <charlestolley@gmail.com>
2021-01-20 06:31:58 -0700

    Created Path class to simplify iteration

--- restapi/path.py
+++ restapi/path.py
@@ -1,26 +1,46 @@
+class Path:
+    class Iterator:
+        def __init__ (self, path):
+            self.path = path
+            self.start = 1
+            self.end = 0
+
+        def __next__ (self):
+            if self.end < 0:
+                raise StopIteration()
+
+            self.end = self.path.find("/", self.start)
+
+            if self.end < 0:
+                segment = self.path[self.start:]
+            else:
+                segment = self.path[self.start:self.end]
+                self.start = self.end+1
+
+            return segment
+
+    def __init__ (self, path):
+        self.path = path
+
+    def __iter__ (self):
+        return self.Iterator(self.path)
+
 class Tree:
     def __init__ (self):
         self.root = {}
 
     def add (self, endpoint, path):
-        if len(path) > 1 and path.endswith("/"):
-            path = path[:-1]
-
         node = self.root
 
-        start = 1
-        while True:
-            end = path.find("/", start)
-            if end < 0:
-                node[path[start:]] = endpoint
-                break
-            else:
-                node = node.setdefault(path[start:end], {})
-                start = end + 1
+        for segment in Path(path):
+            prev = node
+            node = node.setdefault(segment, {})
+
+        prev[segment] = endpoint
 
     def find (self, path):
         node = self.root
-        for segment in path.split("/")[1:]:
+        for segment in Path(path):
             if isinstance(node, dict):
                 try:
                     node = node[segment]
